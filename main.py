5d355710461fadc65fa3b83957ab0b3fe03a8f22
CodeLongAndProsper90 <50145141+CodeLongAndProsper90@users.noreply.github.com>
2019-05-04 16:43:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-from OS import os
-from patch import update
-update()
-os()
-  
+from time import sleep
+try:
+    from OS import os
+    sleep(0.5)
+    print('Loading Salmon OS kernel ')
+    sleep(0.75)
+    print('Version: 0.30')
+    sleep(0.5)
+except:
+    print('Main Kernel loading failed!')
+    sleep(1)
+    choice = input('Load backup Kernel?')
+    if choice == 'y':
+        sleep(0.5)
+        from Backup import os
+        print('Loading backup Kernel')
+        print('Version 0.30')
+while True:
+    os()
