4a5507ffdaae944e8d222e4de1edb292cb64331b
CodeLongAndProsper90 <50145141+CodeLongAndProsper90@users.noreply.github.com>
2019-05-04 16:43:10 -0500

    Update OS.py

--- OS.py
+++ OS.py
@@ -1,58 +1,79 @@
 def os():
-  from time import sleep, time
-  from patch import update
-  commands = ['exit','time' , '', 'help','man', 'version', 'nano', 'pi', 'update']
-  command = input('Scott' +"@salmonOs ~$")
-  def console():
-    def copy(image, file):
-      os = open(file, 'w')
-      os.write('')
-      os.close()
-      os = open(file, 'a')
-      updte = open(image, 'r')
-      for line in updte:
-        patch = updte.readline()
-        os.write(patch)
-    def error():
-      print("ERROR 001")
-    if command in commands:
-      if command == commands[0]:
-        print('stopping Salmon Kernel')
-        sleep(0.5)
-        print("Exiting trinket.io interface")
-        exit()
-      elif command == commands[1]:
-        print("Salmon has encountered an error and needs to restart.")
-      elif command == commands[3]:
-        print("Salmon Os commands")
-        print(commands)
-      elif command == commands[4]:
-        man()
-      elif command == commands[5]:
-        version = 0.26
-        print(version)
-      elif command == commands[7]:
-        print("3.1415265358979232846264338327950288419716939937510582097494450781640628620")
-      elif command == commands[8]:
-        update()
-    if command not in commands:
-      print("Bash: command " + command +" not found")
-    def man():
-      page = input("What command to look up?")
-      if page in commands:
-        if page == commands[0]:
-          print("exit Salmon OS")
-        #elif page == commands[1]:
-        elif page == commands[2]: 
-          print("Returns the system archecture")
-        elif page == commands[3]:
-          print("Print the available commands")
-        elif page == commands[4]:
-          print("You're here!")
-        elif page == commands[5]:
-          print("Prints the Current Salmon verson")
-        elif page == commands[6]:
-          print("Salmon Os file editor")
-  while True:
+    from time import sleep
+    from os import uname
+    platform = []
+    platform = uname()
+    op = platform[0]
+    username = platform[1]
+    arch = platform[4]
+
+    #Insert your name down here |
+    #          V------------------
+    user = 'Scott'
+    commands =['exit', 'time', 'help', 'man', 'version' ,'pi', 'halt', 'arch',]
+    command = input(user +"@salmonOs ~$")
+    def console():
+        def man():
+          page = input("What command to look up?")
+          if page in commands:
+            if page == commands[0]:
+              print("Halt Salmon OS")
+            elif page == commands[1]: 
+              print("Returns the time")
+            elif page == commands[2]:
+              print("Print the available commands")
+            elif page == commands[3]:
+              print("You're here!")
+            elif page == commands[4]:
+              print("Prints the Current Salmon verson")
+            elif page == commands[5]:
+              print("Prints pi")
+            elif page == commands[6]:
+              print('Use this command to force a system halt')
+            elif page == commands[7]:
+              print("Gives you info about the system Salmon os is running on.")
+        def error(message, number):
+            
+          print("ERRORNO " + number + ':' + message)
+        if command in commands:
+          if command == commands[0]:
+            print('stopping Salmon Kernel')
+            sleep(0.5)
+            print('Complete!')
+            sleep(0.25)
+            print("Terminating Fb")
+            sleep(0.25)
+            print("Complete!")
+            sleep(0.75)
+            exit()
+          elif command == commands[1]:
+            error('Salmon has encountered a fatal execption and needs to restart', '004')
+            sleep(3)
+            exit()
+          elif command == commands[2]:
+            print("Salmon Os commands")
+            print(commands)
+          elif command == commands[3]:
+            man()
+          elif command == commands[4]:
+            version = 0.30
+            print(version)
+          elif command == commands[5]:
+            print("3.1415265358979232846264338327950288419716939937510582097494450781640628620")
+          elif command == commands[6]:
+            print('stopping Salmon Kernel')
+            sleep(0.5)
+            print('Complete!')
+            sleep(0.25)
+            print("Terminating Fb")
+            sleep(0.25)
+            print("Complete!")
+            sleep(0.75)
+            exit()
+          elif command == commands[7]:
+            print('OS: ' + op + '; User: ' + username  + '; Arch: ' + arch)
+        if command not in commands:
+          print("Salmon Shell Emulator Protocal: command " + command +" not found")
+
     console()
 
