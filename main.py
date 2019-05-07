f6d80970203ea382276e5ecad8bbb6789dc277ec
CodeLongAndProsper90 <50145141+CodeLongAndProsper90@users.noreply.github.com>
2019-05-07 11:43:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,36 @@
 from time import sleep
+print("Use 'help' for commands")
 try:
     from OS import os
-    sleep(0.5)
     print('Loading Salmon OS kernel ')
     sleep(0.75)
+    sleep(0.5)
     print('Version: 0.30')
     sleep(0.5)
 except:
+  
     print('Main Kernel loading failed!')
     sleep(1)
     choice = input('Load backup Kernel?')
-    if choice == 'y':
+    choices = ['yes', 'no']
+    while choice not in choices:
+      print('yes or no')
+      choice = input('Load backup Kernel?')
+
+    if choice == 'yes':
         sleep(0.5)
-        from Backup import os
+        from update import os
         print('Loading backup Kernel')
         print('Version 0.30')
+    elif choice == 'no':
+      print('Loading aborted!')
+      sleep(1)
+      print("Salmon os has encountered a fatal execption and needs to be repaired or redownloaded! ")
+      sleep(1)
+      print("DANGER! The salmon Os kernel is damaged or missing! DANGER")
+      sleep(2)
+      exit()
+      
+
 while True:
     os()
