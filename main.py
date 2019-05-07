c937989c8c8663057a38831a0992ba02679ae7d9
CodeLongAndProsper90 <50145141+CodeLongAndProsper90@users.noreply.github.com>
2019-05-07 17:19:20 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
 from time import sleep
+login = False
+version = '0.4.6'
 print("Use 'help' for commands")
 user = input("Username?")
+users = ['scott', 'crash']
+user = user.lower()
+passwords = ['8t8mph', 'Fish']
+if user in users:
+  password = input('Password?')
+  if user == users[0]:
+    if password == passwords[1]:
+      login = True
+    else:
+      login == False
+      exit()
+  if user == users[1]:
+    if password == passwords[0]:
+      login = True
+    else:
+      login = False
+      exit()
+else:
+  login = False
+  print("You are not in the list of Comrads of Salmon OS")
+  exit()
 try:
     from kernel import os
     print('Loading Salmon OS kernel ')
     sleep(0.75)
     sleep(0.5)
-    print('Version: 0.30')
+    print('Version ' + version)
     sleep(0.5)
 except:
   
@@ -22,7 +45,7 @@ except:
         sleep(0.5)
         from update import os
         print('Loading backup Kernel')
-        print('Version 0.30')
+        print('Version ' + version)
     elif choice == 'no':
       print('Loading aborted!')
       sleep(1)
@@ -33,5 +56,5 @@ except:
       exit()
       
 
-while True:
-    os(user)
+while login != False:
+    os(user, version)
